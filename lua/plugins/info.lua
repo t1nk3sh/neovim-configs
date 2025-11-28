884001bbe15d5b83ac1d1dce08aa9c4f5041c696
t1nk3sh <barapatretinkesh@gmail.com>
2025-11-28 10:32:14 +0530

    initial configs for neovim

---
+++ lua/plugins/info.lua
@@ -0,0 +1,6 @@
+-- All the plugins which are used is from a youtube channel
+-- and the github repo link is :  https://github.com/hendrikmi/neovim-kickstart-config
+-- by Henry Misc
+--
+-- Big Thanks to kickstart.nvim
+return {}
